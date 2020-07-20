a55c1454b3c28027812eaec081c78ecd8594c6c3
Dominik Magdaleński <dominik.magdalenski@gmail.com>
2020-07-20 21:05:35 +0200

    Add Company, Address and BankAccount models

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 from django.contrib.admin import AdminSite, ModelAdmin
 
-from .models import Invoice
+from .models import Invoice, Company, Address, BankAccount
 
 
 class FabrykaFakturAdminSite(AdminSite):
@@ -16,4 +16,19 @@ class InvoiceAdmin(ModelAdmin):
     list_display = ('id', 'name', 'issue_date', 'sale_date')
 
 
+class CompanyAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'tax_id')
+
+
+class AddressAdmin(ModelAdmin):
+    list_display = ('id', 'country', 'city', 'zip_code', 'street', 'street_number')
+
+
+class BankAccountAdmin(ModelAdmin):
+    list_display = ('id', 'bank_name', 'bank_account_number')
+
+
 site.register(Invoice, InvoiceAdmin)
+site.register(Company, CompanyAdmin)
+site.register(Address, AddressAdmin)
+site.register(BankAccount, BankAccountAdmin)
--- src/models.py
+++ src/models.py
@@ -5,6 +5,13 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
+from pycountry import countries
+
+
+COUNTRIES = [c.name for c in countries]
+COUNTRIES = zip(COUNTRIES, COUNTRIES)
+POLAND = countries.get(alpha_2='PL').name
+
 
 class User(AbstractUser):
     class Meta:
@@ -27,3 +34,47 @@ class Invoice(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Company(models.Model):
+    class Meta:
+        db_table = 'company'
+        verbose_name = _('Company')
+        verbose_name_plural = _('Companies')
+
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
+    tax_id = models.CharField(max_length=100, verbose_name=_('Tax ID'))
+    address = models.ForeignKey('Address', on_delete=models.SET_NULL, null=True, verbose_name=_('Address'))
+    bank_account = models.ForeignKey('BankAccount', on_delete=models.SET_NULL, default=None, null=True, blank=True, verbose_name=_('Bank account'))
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    class Meta:
+        db_table = 'address'
+        verbose_name = _('Address')
+        verbose_name_plural = _('Addresses')
+
+    country = models.CharField(max_length=50, choices=COUNTRIES, default=POLAND, verbose_name=_('Country'))
+    city = models.CharField(max_length=100, verbose_name=_('City'))
+    zip_code = models.CharField(max_length=100, verbose_name=_('ZIP Code'))
+    street = models.CharField(max_length=100, verbose_name=_('Street'))
+    street_number = models.CharField(max_length=100, verbose_name=_('Street number'))
+
+    def __str__(self):
+        return f'{self.city} {self.zip_code}, {self.street} {self.street_number}'
+
+
+class BankAccount(models.Model):
+    class Meta:
+        db_table = 'bank_account'
+        verbose_name = _('Bank Account')
+        verbose_name_plural = _('Bank Accounts')
+
+    bank_name = models.CharField(max_length=100, default='', blank=True, verbose_name=_('Bank name'))
+    bank_account_number = models.CharField(max_length=32, verbose_name=_('Bank account number'))
+
+    def __str__(self):
+        return f'{self.bank_name}, {self.bank_account_number}'
